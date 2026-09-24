a44aff31e1347fb9f405cbae947ddab2aa1dd647
isarodpra <isarodpra@gmail.com>
2026-09-24 11:32:12 -0600

    Create fase2ddl_v0.sql
    Registro de sql del esqueleto

---
+++ fase2/fase2ddl_v0.sql
@@ -0,0 +1,38 @@
+-- Esqueleto del esquema logico --
+
+CREATE TABLE libros (
+  isbn              VARCHAR(20)  PRIMARY KEY,
+  titulo            VARCHAR(200) NOT NULL,
+  autor             VARCHAR(150) NOT NULL,
+  editorial         VARCHAR(100),
+  anio_publicacion  INTEGER
+);
+
+CREATE TABLE usuarios (
+  id_usuario    INTEGER      PRIMARY KEY,
+  nombre        VARCHAR(150) NOT NULL,
+  telefono      VARCHAR(20),
+  tipo_usuario  VARCHAR(20),
+  contrasena    VARCHAR(50)  NOT NULL,
+  correo        VARCHAR(150) NOT NULL UNIQUE
+);
+
+CREATE TABLE ejemplares (
+  id_ejemplar       INTEGER      PRIMARY KEY,
+  ubicacion_fisica  VARCHAR(100),
+  estado            VARCHAR(20),
+  isbn              VARCHAR(20)  NOT NULL REFERENCES libros(isbn)
+  -- , UNIQUE (isbn)  -- descomentar si LIBROS–EJEMPLARES es 1:1
+);
+
+CREATE TABLE prestamos (
+  id_prestamo       INTEGER      PRIMARY KEY,
+  fecha_prestamo    DATE         NOT NULL,
+  fecha_reserva     DATE,
+  fecha_devolucion  DATE,
+  monto             NUMERIC(10,2),
+  estatus           VARCHAR(20),
+  id_usuario        INTEGER      NOT NULL REFERENCES usuarios(id_usuario),
+  id_ejemplar       INTEGER      NOT NULL REFERENCES ejemplares(id_ejemplar)
+);
+
